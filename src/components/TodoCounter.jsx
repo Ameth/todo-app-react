065d63f55d89680b06755acba41e72f7841c138b
Ameth Ordoñez <amethgabriel@hotmail.com>
2023-04-29 09:25:19 -0500

    Fix text in counter

--- src/components/TodoCounter.jsx
+++ src/components/TodoCounter.jsx
@@ -1,5 +1,5 @@
 function TodoCounter({total, completed}) {
-  return <h2 className='text-2xl text-teal-500 font-bold my-2'>Completed {completed} to {total}</h2>
+  return <h2 className='text-2xl text-teal-500 font-bold my-2'>Completed {completed} of {total}</h2>
 }
  
 export { TodoCounter }
